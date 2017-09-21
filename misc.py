813cfb4876f2e7af13c69542cda67fce9d78a029
yankobogdan <yankobogdan@gmail.com>
2017-09-21 22:24:38 +0300

    adding misc file

---
+++ misc.py
@@ -0,0 +1,7 @@
+token = '447767383:AAEz6qweNAmG6RA32iVnPuoblQvl3at918s'
+static_chat_Andrii = 291376558
+static_chat_Denis = 381956051
+static_chat_Alexander = 176097832
+static_chat_Alexander2 = 3994615
+static_chat_Group = -185802751
+static_chat_dezhurnii = 307497958
